54da5a7f91219e2345e319ab11b53341bd67d7db
loforteluca <lucaloforte3@gmail.com>
2022-05-03 18:09:18 -0400

    Create getPokemonDescription controller function

--- server/api/pokemon/index.js
+++ server/api/pokemon/index.js
@@ -1,9 +1,11 @@
 // Libraries
 import express from "express"
+// Files
+import { getPokemonDescription } from "../../controllers/pokemon.js"
 
 const router = express.Router()
 router.get("/:pokemonName", (req, res, next) => {
-  res.json({name: "Pokemon Name", description: "Pokemon Description"})
+  getPokemonDescription(req, res, next)
 })
 
 export default router
---
+++ server/controllers/pokemon.js
@@ -0,0 +1,6 @@
+export const getPokemonDescription = (req, res, next) => {
+  // validate input
+  // fetch description
+  // create response object
+  res.json({name: "Pokemon Name", description: "Pokemon Description"})
+}
