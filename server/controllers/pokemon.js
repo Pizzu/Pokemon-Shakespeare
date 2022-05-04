34bcc6152921ffbbef5858323beeb44b61f1c554
loforteluca <lucaloforte3@gmail.com>
2022-05-04 11:56:07 -0400

    Translate pokemon description

--- server/controllers/pokemon.js
+++ server/controllers/pokemon.js
@@ -10,11 +10,16 @@ export const getPokemon = async (req, res, next) => {
     const pokemonDescription = pokemonSpecies.flavor_text_entries[0].flavor_text.replace(/[\n\r\f]+/g, ' ')
   
     // Translate description
+    const pokemonTranslation = await axios.post("https://api.funtranslations.com/translate/shakespeare.json", 
+      {
+        text: pokemonDescription
+      }
+    ).then(res => res.data)
 
     // Create response object
     res.status(200).json({
       name: pokemon.name,
-      description: pokemonDescription
+      description: pokemonTranslation.contents.translated
     })
   } catch (error) {
     // Errors will be handled by an ErrorHandler middleware
