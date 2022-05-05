e0af9cb5ee651359b9ce2e6f8f2f148957b82e21
loforteluca <lucaloforte3@gmail.com>
2022-05-05 17:37:19 -0400

    Install and configure tailwind css, create base folder structure for the client

---
+++ client/tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+		/* Add colors property to replace default palette */
+    container: {
+      center: true,
+      padding: "5rem"
+    },
+    screens: {
+      xl: "1650px",
+      lg: { "max": "991px" },
+      md: { "max": "767px" },
+      sm: { "max": "479px" }
+    },
+    extend: {
+      fontFamily: {
+        "nunito-sans": ["Nunito Sans"]
+      },
+      fontSize: {
+        'html-xl': "18.333333333333336px",
+        'html-base': "1.1111111111111112vw",
+        'html-lg': "11.011111111111111px",
+        'html-md': "8.622222222222222px",
+        'html-sm': "5.322222222222223px",
+        "caption": "1.13rem",
+        "nav-link": "1.25rem"
+      },
+      colors: {
+        "primary": "#FF6B4A",
+        "secondary": "#2ABEDF",
+        "grey-light": "#F7F7F7",
+        "grey-dark": "#999999"
+      }
+    },
+  }
+}
+
