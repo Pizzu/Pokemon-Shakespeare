d218f6d182bd8c52a142ffa2369daeb16ccba618
loforteluca <lucaloforte3@gmail.com>
2022-05-06 00:08:24 -0400

    Create Hero and Button components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { SectionLayout } from "./components/common";
+import { SectionLayout, Hero, Button } from "./components/common";
 
 const App: React.FC = () => {
+
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log("Click")
+  }
+
   return (
     <main>
       <SectionLayout>
         <div className="grid grid-flow-row gap-10 max-w-3xl mx-auto">
-          <div className="text-center">
+          <Hero>
             <h1>Pokemon Shakespeare</h1>
             <p>Type on the search field a pokemon name</p>
-          </div>
-          <form className="grid grid-cols-7 items-stretch gap-6">
+          </Hero>
+          <form onSubmit={onFormSubmit} className="grid grid-cols-7 items-stretch gap-6">
             <label htmlFor="pokemon" className="block col-span-5">
               <input type="text" id="pokemon" name="pokemon" className="h-full px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-primary focus:ring-primary block w-full rounded-lg focus:ring-1"
                 placeholder="Charizard, Bulbasaur, Wartortle ..." />
             </label>
             <div className="col-span-2">
-              <button className="nav-link bg-primary text-white rounded-lg px-6 py-3">Search Now</button>
+              <Button type="submit" className="bg-primary text-white">Search Now</Button>
             </div>
           </form>
         </div>
--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -1,1 +1,3 @@
 export { default as SectionLayout } from "./section" 
+export { default as Hero } from "./hero"
+export { default as Button } from "./button"
